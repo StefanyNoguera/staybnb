b761e0bfaba3dc6213145d31e8c3c6ce19002e3d
Stefany Noguera <stefanynoguerar@gmail.com>
2025-04-25 13:30:52 +0200

    allowed users to sign up in views and added path to urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PropertyListCreateView
+from .views import PropertyListCreateView, SignupView
 
 urlpatterns = [
-    path('properties/', PropertyListCreateView.as_view(), name='property-list-create')
+    path('properties/', PropertyListCreateView.as_view(), name='property-list-create'),
+    path('signup/', SignupView.as_view(), name='signup'),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions, filters
-from .models import Property
-from .serializers import PropertySerializer
+from .models import Property, User
+from .serializers import PropertySerializer, UserSerializer
+from django.contrib.auth import get_user_model
 
 class PropertyListCreateView(generics.ListCreateAPIView):
     queryset = Property.objects.all().order_by('-created_at')
@@ -12,3 +13,8 @@ class PropertyListCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(host=self.request.user)
+
+class SignupView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
