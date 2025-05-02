9480c8a23c5e005dc34a8f27d9fa377c7a645515
Stefany Noguera <stefanynoguerar@gmail.com>
2025-05-02 17:34:14 +0200

    Protect property creation: require login

--- backend/core/views.py
+++ backend/core/views.py
@@ -3,11 +3,12 @@ from rest_framework import generics, permissions, filters
 from .models import Property, User
 from .serializers import PropertySerializer, SignupSerializer
 from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class PropertyListCreateView(generics.ListCreateAPIView):
     queryset = Property.objects.all().order_by('-created_at')
     serializer_class = PropertySerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filter_backends = [filters.SearchFilter]
     search_fields = ['city', 'country']
 
