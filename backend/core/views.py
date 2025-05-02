2ce4d13aed4c3d15c49f068648bf187dfc71822e
Stefany Noguera <stefanynoguerar@gmail.com>
2025-05-02 17:36:46 +0200

    Restrict property creation to hosts only

--- backend/core/views.py
+++ backend/core/views.py
@@ -4,6 +4,8 @@ from .models import Property, User
 from .serializers import PropertySerializer, SignupSerializer
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.exceptions import PermissionDenied
+
 
 class PropertyListCreateView(generics.ListCreateAPIView):
     queryset = Property.objects.all().order_by('-created_at')
@@ -13,6 +15,8 @@ class PropertyListCreateView(generics.ListCreateAPIView):
     search_fields = ['city', 'country']
 
     def perform_create(self, serializer):
+        if not self.request.user.is_host:
+            raise PermissionDenied("Only hosts can create properties")
         serializer.save(host=self.request.user)
 
 class SignupView(generics.CreateAPIView):
