c98c8eb55e94d535539a0b34f7e478a182bb5b36
Stefany Noguera <stefanynoguerar@gmail.com>
2025-05-08 16:58:51 +0200

    Add /api/bookings/ endpoint for creating and listing guest bookings

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PropertyListCreateView, SignupView, ProfileView
+from .views import PropertyListCreateView, SignupView, ProfileView, BookingListCreateView
 
 urlpatterns = [
     path('properties/', PropertyListCreateView.as_view(), name='property-list-create'),
     path('signup/', SignupView.as_view(), name='signup'),
-    path('profile/', ProfileView.as_view(), name='user-profile')
+    path('profile/', ProfileView.as_view(), name='user-profile'),
+    path('bookings/', BookingListCreateView.as_view(), name='booking-list-create'),
 ]
